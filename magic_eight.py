116228ad25970409e69adcbf963abf3cc632338e
Jocelyn Kuo <jocelynkuo@Jocelyns-Air-2.attlocal.net>
2019-09-17 16:43:16 -0400

    check code

--- magic_eight.py
+++ magic_eight.py
@@ -1,4 +1,11 @@
 # This is the magic_eight.py file
 
-user_input = input("What is your question?")
-print(user_input)
+user_input = input("What is your question? ")
+
+for response in user_input[-1]:
+    if user_input == "?":
+        pass
+    elif user_input == "quit":
+        break
+    else:
+        print("I'm sorry, I can only answer questions")
